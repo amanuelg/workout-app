7422a8ba697d7835fa210a6c0a49cf9709ee3260
amanuelg <ama.ghya@gmail.com>
2016-01-14 16:52:28 +0000

    adding names validation

--- spec/features/signing_up_users_spec.rb
+++ spec/features/signing_up_users_spec.rb
@@ -17,4 +17,20 @@ RSpec.feature "Signing up users" do
         expect(page).to have_content("Jhon Doe")
         
     end
+    
+    scenario "signing up user with invalid credentials" do
+        visit "/"
+        click_link "Sign up"
+        
+        fill_in "First name", with: ""
+        fill_in "Last name", with: ""
+        fill_in "Email", with:"jhone@example.com"
+        fill_in "Password", with:"password"
+        fill_in "Password confirmation", with:"password"
+        click_button "Sign up"
+        
+        expect(page).to have_content("First name can't be blank")
+        expect(page).to have_content("Last name can't be blank")
+        
+    end
 end
