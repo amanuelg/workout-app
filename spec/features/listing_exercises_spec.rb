4fe197ab90b361a0931f4b4ea00ab206eeca852e
amanuelg <ama.ghya@gmail.com>
2016-01-15 21:04:43 +0000

    display friends

--- spec/features/listing_exercises_spec.rb
+++ spec/features/listing_exercises_spec.rb
@@ -3,10 +3,13 @@ require "rails_helper"
 RSpec.feature "listing exercises" do
     before do
         @jhon = User.create!(first_name: "John",last_name: "Doe",email: "jhon@example.com", password: "password")
+        @sara = User.create!(first_name: "Sara",last_name: "Doe",email: "sara@example.com", password: "password")
         login_as(@jhon)
         
         @e1 = @jhon.exercises.create!(:duration_in_min => 60, :workout => "lifting weight", :workout_date => "2016-01-12" )
         @e2 = @jhon.exercises.create!(:duration_in_min => 60, :workout => "Running", :workout_date => "2016-01-11" )
+        
+        @following = Friendship.create!(user: @jhon, friend: @sara)
     end
     
     scenario "listing exercises for the last 7 days" do
@@ -20,6 +23,15 @@ RSpec.feature "listing exercises" do
         expect(page).to have_content(@e2.workout)
         expect(page).to have_content(@e2.workout_date)
         
-        
     end
+    
+    scenario "show a list of users friends" do
+        visit "/"
+        
+        click_link "My Lounge"
+        expect(page).to have_content("My Friends")
+        expect(page).to have_link(@sara.full_name)
+        expect(page).to have_link("Unfollow")
+    end
+    
 end
